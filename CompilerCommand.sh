5f1ce281b4ded15fb9a92cae9fce0daa25a04490
Carlos Mendes <carlosmendes_7@hotmail.com>
2019-09-21 17:18:43 -0300

    Add files via upload

---
+++ CompilerCommand.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+g++ -std=c++11 -Wall -Wextra -pedantic -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector $1
