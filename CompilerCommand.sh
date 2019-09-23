a06fc47619c1ae3803af3ae448300942144ee1e5
Carlos Mendes <carlosmendes_7@hotmail.com>
2019-09-23 07:20:30 -0300

    Update CompilerCommand.sh

--- CompilerCommand.sh
+++ CompilerCommand.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-g++ -std=c++11 -Wall -Wextra -pedantic -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector $1
+g++ -std=c++11 -Wall -Wextra -pedantic -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 
+-Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address 
+-fsanitize=undefined -fno-sanitize-recover -fstack-protector $1
